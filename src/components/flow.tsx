7c276a2ca2289086ec35d685d2beb889f946dd7f
さいかい <70578583+saikai-moviestudio@users.noreply.github.com>
2022-03-15 11:41:48 +0900

    テキスト配置まで試した

---
+++ src/components/flow.tsx
@@ -0,0 +1,23 @@
+import { Flex } from "@chakra-ui/react";
+
+const Flow = () => {
+  return (
+    <Flex
+      w="full"
+      h="full"
+      direction="column"
+      color="white"
+      fontWeight="700"
+      fontSize="36"
+    >
+      <p>テキストが流れます</p>
+      <p>テキストが流れます</p>
+      <p>テキストが流れます</p>
+      <p>テキストが流れます</p>
+      <p>テキストが流れます</p>
+      <p>テキストが流れます</p>
+    </Flex>
+  );
+};
+
+export default Flow;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,18 @@
-import { Flex } from "@chakra-ui/react";
+import Flow from "@/components/flow";
+import { Box, Flex } from "@chakra-ui/react";
 import { NextPage } from "next";
+import Camera from "src/components/camera";
 import Header from "src/components/header";
 
 const Index: NextPage = () => {
   return (
     <Flex direction="column" w="full" h="100vh" px={8}>
       <Header title="MeerChat" />
-      <Flex w="full" direction="column">
-        MeerChat
+      <Flex pos="relative" w="full" direction="column">
+        <Box pos="absolute" top="0" left="0">
+          <Flow />
+        </Box>
+        <Camera />
       </Flex>
     </Flex>
   );

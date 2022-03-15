7ba437530bc544c2cce7fe89c0ce76577275a06f
さいかい <70578583+saikai-moviestudio@users.noreply.github.com>
2022-03-16 00:54:21 +0900

    windowを表示するところまで完了

--- src/pages/audience.tsx
+++ src/pages/audience.tsx
@@ -1,27 +1,35 @@
 import { Flex } from "@chakra-ui/react";
 import { NextPage } from "next";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const Audience: NextPage = () => {
+  const [smallWindow, setSmallWindow] = useState<Window>();
+  const [smallWindowOpened, setSmallWindowOpened] = useState<boolean>(false);
   const openWindow = () => {
     if (screen !== undefined) {
       const width = 480;
       const height = 240;
       const screenX = screen.width - width - 16;
       const screenY = screen.height - height - 64;
-      window.open(
+      const _smallWindow = window.open(
         "http://localhost:3000/audience-display/",
         "audience",
         `width=${width},height=${height},screenY=${screenY},screenX=${screenX},toolbar=no,menubar=no,scrollbars=no`
       );
+      _smallWindow!.blur();
+      _smallWindow!.focus();
+      setSmallWindow(_smallWindow as Window);
     }
   };
-  openWindow();
 
   useEffect(() => {
-    openWindow();
-    // TODO: なんとかしてタブを閉じたい
-    // window.open("about:blank", "_self", "")?.close();
+    // TODO: リスタート時にタブが復活する設定を回避できたら嬉しい
+    if (!smallWindowOpened) {
+      openWindow();
+      setSmallWindowOpened(true);
+    }
+    window.close();
+    // window.open("", "_self", "")?.close();
   }, []);
 
   return (
--- src/pages/speaker.tsx
+++ src/pages/speaker.tsx
@@ -1,27 +1,34 @@
 import { Flex } from "@chakra-ui/react";
 import { NextPage } from "next";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const Speaker: NextPage = () => {
+  const [smallWindow, setSmallWindow] = useState<Window>();
+  const [smallWindowOpened, setSmallWindowOpened] = useState<boolean>(false);
   const openWindow = () => {
     if (screen !== undefined) {
       const width = 360;
       const height = 160;
       const screenX = 0;
       const screenY = 0;
-      window.open(
+      const _smallWindow = window.open(
         "http://localhost:3000/speaker-display/",
         "speaker",
         `width=${width},height=${height},screenY=${screenY},screenX=${screenX},toolbar=no,menubar=no,scrollbars=no`
       );
+      _smallWindow!.blur();
+      _smallWindow!.focus();
+      setSmallWindow(_smallWindow as Window);
     }
   };
-  openWindow();
 
   useEffect(() => {
-    openWindow();
-    // TODO: なんとかしてタブを閉じたい
-    // window.open("about:blank", "_self", "")?.close();
+    // TODO: リスタート時にタブが復活する設定を回避できたら嬉しい
+    if (!smallWindowOpened) {
+      openWindow();
+      setSmallWindowOpened(true);
+    }
+    window.close();
   }, []);
 
   return (
